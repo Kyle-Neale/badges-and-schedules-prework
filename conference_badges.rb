ec940afe3a586d4d3f422470ed1aa2f49623981f
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-02 21:07:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 
 def assign_room(speakers)
   room_assignments = []
-  speakers.each { |speaker| room_assignments.push("Hello, #{_____}! You'll be assigned to room _____!")
-  
+  speakers.each { |speaker| room_assignments.push("Hello, #{_____}! You'll be assigned to room #{speakers.index(speaker}")
+  room_assignments
 end
   
