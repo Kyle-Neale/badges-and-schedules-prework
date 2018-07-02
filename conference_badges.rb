d0bfc82f2812355376bbf4c54c96f9418116aa65
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-02 21:01:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,13 +4,14 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-badge_message = []
+
 def batch_badge_creator(arr)
+  badge_message = []
   arr.each { |name| badge_message.push(badge_maker(name)) }
+  badge_message
 end
 
-rooms = []
-room_output = []
+
 def assign_room(speaker)
   rooms.push(speaker)
   
